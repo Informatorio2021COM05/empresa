1cd656b6b739a9c66da8830ad83384f3bd9edd83
user.name <denysiuk.isi@gmail.com>
2021-02-17 11:42:37 -0300

    models departamento y empleado creados

--- empleado/models.py
+++ empleado/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from departamento.models import Departamento
 # Create your models here.
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length = 50)
+    apellido = models.CharField(max_length = 50, null=True)
+    nacimiento = models.DateField()
+    sueldo = models.DecimalField(max_digits=10, decimal_places=2)
+    departamento = models.ForeignKey(Departamento,
+        on_delete=models.SET_NULL,
+        null=True)
